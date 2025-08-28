74db15524915c39ff15c0f23537a76712820229f
myname <thunyanan0020@gmail.com>
2025-08-28 02:34:13 +0000

    finish

--- polls/models.py
+++ polls/models.py
@@ -1,8 +1,8 @@
+import datetime
+
 from django.db import models
 from django.utils import timezone
-from datetime import timedelta
-
-# Create your models here.
+from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,14 +11,19 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+    boolean=True,
+    ordering="pub_date",
+    description="Published recently?",
+    )
+    
     def was_published_recently(self):
         now = timezone.now()
-        return now - timedelta(days=1) <= self.pub_date <= now
-    was_published_recently.boolean = True
-    was_published_recently.short_description = "Published recently?"
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="choices")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
